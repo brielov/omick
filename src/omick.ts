e36107839ae2423f70c19dd4e331ba48e2c5f14c
Gabriel Vaquer <brielov@icloud.com>
2022-06-14 09:29:37 -0300

    Revert to older technique for iterating keys

--- src/omick.ts
+++ src/omick.ts
@@ -1,23 +1,29 @@
-type PlainObject = { [key: string]: unknown };
-
 /**
  * Create a new object containing only the specified keys
  */
-export const pick = <T extends PlainObject, K extends keyof T>(
+export function pick<T extends object, K extends keyof T>(
   obj: T,
   ...keys: K[]
-): Pick<T, K> =>
-  Object.fromEntries(
-    Object.entries(obj).filter(([key]) => keys.includes(key as K)),
-  ) as Pick<T, K>;
+): Pick<T, K> {
+  const result = Object.create(null);
+  for (const key of keys) {
+    result[key] = obj[key];
+  }
+  return result;
+}
 
 /**
  * Create a new object excluding the specified keys
  */
-export const omit = <T extends PlainObject, K extends keyof T>(
+export function omit<T extends object, K extends keyof T>(
   obj: T,
   ...keys: K[]
-): Omit<T, K> =>
-  Object.fromEntries(
-    Object.entries(obj).filter(([key]) => !keys.includes(key as K)),
-  ) as Omit<T, K>;
+): Omit<T, K> {
+  const result = Object.create(null);
+  for (const k in obj) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (keys.includes(k as any)) continue;
+    result[k] = obj[k];
+  }
+  return result;
+}
